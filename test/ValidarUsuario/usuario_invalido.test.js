c0a52637063f0d3faca71f84003ef19eea27605b
Gislaine Melo [ Zallpy ] <gislaine.melo@zallpy.com>
2025-10-02 00:27:04 -0300

    Correção de arquivos de validação d usuario

--- test/ValidarUsuario/usuario_invalido.test.js
+++ test/ValidarUsuario/usuario_invalido.test.js
@@ -2,22 +2,26 @@
 const request = require('supertest'); //requisicoes de API
 const {expected, expect} = require('chai');
 
-//Testes
 describe ('Login',() => {    
-    it ('Deve retornar 200 com credencias validas', async () => {
+    it ('Deve retornar 400 com credencias invalidas', async () => {
     describe ('POST/users/login',() => {
        before(async () => {
              const postLogin = require('../fixture/requisicoes/login/postLogin.json');
            
                        const respostaLogin = await request(process.env.BASE_URL_REST)
                            .post('/users/login')
-                           .send(postLogin);
+                           .send({
+                            "username": "julio",
+                            "senha": "12345"
+
+                           });
            
                        token = respostaLogin.body.token;
-                        expect(respostaLogin.status).to.equal(200);
+                       expect(respostaLogin.status).to.equal(400);
                    });
 
         });
 
     });
-}); 
+
+});
